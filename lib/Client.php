ba5c99e9fd8c12b41b79b387670575385f439fdb
SIGUI kessé Emmanuel <sikessem@gmail.com>
2021-06-30 02:59:47 +0200

    Create the client class

---
+++ lib/Client.php
@@ -0,0 +1,12 @@
+
+<?php namespace SIKessEm\HTTP;
+
+/**
+ * The client class
+ *
+ * @author SIGUI Kessé Emmanuel
+ * @package sikessem/http
+ * @license Apache-2.0
+ */
+class Client extends Messenger {
+}
