f1b57dfc1f2fdb5e298667d9f4bbbb8c478552cf
SIGUI kessé Emmanuel <sikessem@gmail.com>
2021-06-30 03:28:09 +0200

    Add commands handling from the client class

--- lib/Client.php
+++ lib/Client.php
@@ -9,4 +9,31 @@
  * @license Apache-2.0
  */
 class Client extends Messenger {
+
+  /**
+   * Create a new client
+   *
+   * @param string $ip The client IP address
+   * @param int $port The port used by the client
+   * @param array $handlers Commands handlers list
+   */
+  public function __construct(string $ip, int $port, array $handlers = []) {
+    parent::__construct($ip, $port);
+    $this->cart = new Cart($this, $handlers);
+  }
+
+  /**
+   * @var Cart The client cart
+   */
+  protected Cart $cart;
+
+  /**
+   * Listen to client order : create a new command and add it to the client cart
+   *
+   * @param callable $callback The order handler
+   * @return Command The command created
+   */
+  public function onOrder(callable $handler): Command {
+    return $this->cart->add($handler);
+  }
 }
