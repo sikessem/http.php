3c8c6cc24949c8b65ac4f5bee7564e4da4379cba
SIGUI kessé Emmanuel <sikessem@gmail.com>
2021-06-30 03:28:37 +0200

    Add services handling from the server class

--- lib/Server.php
+++ lib/Server.php
@@ -8,4 +8,36 @@
  * @license Apache-2.0
  */
 class Server extends Messenger {
+
+  /**
+   * @var Menu The server menu
+   */
+  protected Menu $menu;
+
+  /**
+   * Create a new server
+   *
+   * @param string $ip The server IP address
+   * @param int $port The port used by the server
+   * @param array $handlers Services handlers list
+   */
+  public function __construct(string $ip, int $port, array $handlers = []) {
+    parent::__construct($ip, $port);
+    $this->card = new Menu($this, $handlers);
+  }
+
+  /**
+   * @var Menu The server menu
+   */
+  protected Menu $menu;
+
+  /**
+   * Listen to server service : create a new service and add it to the server menu
+   *
+   * @param callable $callback The order handler
+   * @return Service The service created
+   */
+  public function onServe(callable $handler): Service {
+    return $this->menu->add($handler);
+  }
 }
